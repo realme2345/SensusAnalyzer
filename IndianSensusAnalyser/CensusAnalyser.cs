4c5fd22b81d26b7aff004863a114fa7056e13fbe
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-13 14:29:09 +0530

    [Raja]Add Indian Sensus Analyser from the CSV File

---
+++ IndianSensusAnalyser/CensusAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using IndianSensusAnalyser.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianSensusAnalyser
+{
+    internal class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA, US
+        }
+        public Dictionary<string, CensusDTO> datamap;
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            datamap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return datamap;
+        }
+    }
+}
+}
